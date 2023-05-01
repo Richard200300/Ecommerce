5b830cd9a14901736405143ecde6af76dcb99a76
Richard200300 <103750454+Richard200300@users.noreply.github.com>
2023-05-01 01:08:48 +0100

    update

--- vite-project/src/body.jsx
+++ vite-project/src/body.jsx
@@ -5,12 +5,12 @@ function Body() {
   const [productImg, setProductImg] = useState(0);
   const [items, setItems] = useState(0);
   const [data, setData] = useState({
-    price: 125.00,
+    price: 125.0,
     total: "",
-    productName : "Fall Limited Edition Sneakers",
+    productName: "Fall Limited Edition Sneakers",
     productDescription: `  These low-profile sneakers are you rperfect casual wear
     companion.Featuring a durable rubber outer Sole they'll withstand
-    everything the weather can offer.`
+    everything the weather can offer.`,
   });
   const img = [
     "image-product-1.jpg",
@@ -18,23 +18,19 @@ function Body() {
     "image-product-3.jpg",
     "image-product-4.jpg",
   ];
-function handlecart() {
-  setData({...data,
-     total :  items * data.price})
-}
+  function handlecart() {
+    setData({ ...data, total: items * data.price });
+  }
 
   return (
     <div className="body-section">
-      <Navbar 
-      data={data}
-      setData={setData}items={items}
-      />
+      <Navbar data={data} setData={setData} items={items} />
       <section className="flex main-cont">
         <section className="image-container">
           <section>
             <img src={img[productImg]} className="product-img" />
           </section>
-        
+
           <section className="flex thumbnail-cont">
             <div
               onClick={() => {
@@ -95,45 +91,39 @@ function handlecart() {
 
         <section className="description-section">
           <h2>sneaker company</h2>
-          <h1>{data.productName}</h1>
+          <h1 className="productName">{data.productName}</h1>
 
-          <p>
-          {data.productDescription}
-          </p>
+          <p>{data.productDescription}</p>
           <div>
             <p>$125.00</p>
             <p>50%</p>
           </div>
           <p>$250</p>
-          <div>
-            {items === 0 ? (
-              <button>-</button>
-            ) : (
-              <button
-                onClick={() => {
-                  setItems(items - 1);
-                }}
-              >
-                -
-              </button>
-            )}
-            <p>{items}</p>
-            {items === img.length ? (
-              <button>+</button>
-            ) : (
-              <button
-                onClick={() => {
-                  setItems(items + 1);
-                }}
-              >
-                +
-              </button>
-            )}
+          <div className="flex atc">
+          <div className=" item-cont">
+            <div
+              className="add-btn"
+              onClick={() => {
+                items === 0 ? setItems(items) : setItems(items - 1);
+              }}
+            >
+              <img src="icon-minus.png" alt="icon-plus.png" />
+            </div>
+
+            <p className="item-num">{items}</p>
+            <div
+              className="sub-btn"
+              onClick={() => {
+                items === img.length ? setItems(items) : setItems(items + 1);
+              }}
+            >
+              <img src="icon-plus.png" alt="icon-plus.png" />
+            </div>
           </div>
-          <button onClick={handlecart}>Add to cart</button>
-          {items} * {data.price}  =  {data.total}
+          <button onClick={handlecart} className="checkout-btn ">Add to cart</button>
+          </div>
+          
         </section>
-        
       </section>
     </div>
   );
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Body from "./body";
 import "../src/style/index.css";
+import "./style/fonts/kumbh-sans-font.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -10,3 +11,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </h1>
   </React.StrictMode>
 );
+
